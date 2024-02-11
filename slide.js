060bcdc6b2e8b4377a4fee6d559a4e745071a54b
nucikk <kirianuci@gmail.com>
2024-02-11 12:26:33 +0400

    add calculator functionality

--- slide.js
+++ slide.js
@@ -1,27 +1,9 @@
-let sliderData = [
-    {
-        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvellum.myshopify.com%2Fcdn%2Fshop%2Fproducts%2FOwl-IMG_3152.jpg%3Fv%3D1432665578&tbnid=496nHYAE8Xw0uM&vet=12ahUKEwjC3syaze6DAxVugf0HHTjQBdMQMygAegQIARBO..i&imgrefurl=https%3A%2F%2Fvellum.myshopify.com%2Fproducts%2F600px-width&docid=gz9menbThGUW8M&w=600&h=400&q=600px%20image&client=opera-gx&ved=2ahUKEwjC3syaze6DAxVugf0HHTjQBdMQMygAegQIARBO",
-        siteUrl: "https://medium.com/",
-        title: "Title 1 "
-    },
-    {
-        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvellum.myshopify.com%2Fcdn%2Fshop%2Fproducts%2FOwl-IMG_3152.jpg%3Fv%3D1432665578&tbnid=496nHYAE8Xw0uM&vet=12ahUKEwjC3syaze6DAxVugf0HHTjQBdMQMygAegQIARBO..i&imgrefurl=https%3A%2F%2Fvellum.myshopify.com%2Fproducts%2F600px-width&docid=gz9menbThGUW8M&w=600&h=400&q=600px%20image&client=opera-gx&ved=2ahUKEwjC3syaze6DAxVugf0HHTjQBdMQMygAegQIARBO",
-        siteUrl: "https://medium.com/",
-        title: "Title 2 "
-    },
-    {
-        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvellum.myshopify.com%2Fcdn%2Fshop%2Fproducts%2FOwl-IMG_3152.jpg%3Fv%3D1432665578&tbnid=496nHYAE8Xw0uM&vet=12ahUKEwjC3syaze6DAxVugf0HHTjQBdMQMygAegQIARBO..i&imgrefurl=https%3A%2F%2Fvellum.myshopify.com%2Fproducts%2F600px-width&docid=gz9menbThGUW8M&w=600&h=400&q=600px%20image&client=opera-gx&ved=2ahUKEwjC3syaze6DAxVugf0HHTjQBdMQMygAegQIARBO",
-        siteUrl: "https://medium.com/",
-        title: "Title 3 "
-    },
-    {
-        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fvellum.myshopify.com%2Fcdn%2Fshop%2Fproducts%2FOwl-IMG_3152.jpg%3Fv%3D1432665578&tbnid=496nHYAE8Xw0uM&vet=12ahUKEwjC3syaze6DAxVugf0HHTjQBdMQMygAegQIARBO..i&imgrefurl=https%3A%2F%2Fvellum.myshopify.com%2Fproducts%2F600px-width&docid=gz9menbThGUW8M&w=600&h=400&q=600px%20image&client=opera-gx&ved=2ahUKEwjC3syaze6DAxVugf0HHTjQBdMQMygAegQIARBO",
-        siteUrl: "https://medium.com/",
-        title: "Title 4 "
-    }
 
-]
-let slider = document.getElementById("slider");
-let right = document.getElementById("right");
-let left = document.getElementById("left");
+function calculate() {
+    let firstNumber = document.getElementById("number-first").value;
+    let secondNumber = document.getElementById("number-second").value;
 
+
+    document.getElementById("result").innerText = parseInt(firstNumber) + parseInt(secondNumber)
+
+}
